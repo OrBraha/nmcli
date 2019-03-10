23e49a52c4d2d2cea91d45cb790c3ef8cef608f6
or <or@or.mynet>
2019-03-10 14:16:32 +0200

    update

--- nmclistuff.sh
+++ nmclistuff.sh
@@ -7,18 +7,22 @@
 #
 #
 #
-echo "NMCLI Configuration Tool - "
-nmcli dev status | awk ' {print $1} '
+## Display available devices to user.
+echo "Displaying available devices:  "
+echo "------------------------------------"
+nmcli dev status | awk ' {print   $1 } ' | grep -v -i "Device" 
+echo "------------------------------------"
+# Ask user to config.
 read -p "Please type device to configure: " DEVICE
 read -p "Please type profile name: " PROFILENAME
 read -p "Please type the connection type: " TYYPE
 read -p "Please type IPV4 i.e 1.2.3.4/24 " IPV4
 read -p "Please type gateway: " GATEWAY
 read -p "Please type DNS: " DNSS
-sleep 0.5
 nmcli connection add type $TYYPE con-name $PROFILENAME ifname $DEVICE ip4 $IPV4 gw4 $GATEWAY
 nmcli con mod $PROFILENAME ipv4.dns $DNSS
 echo "Configuartion completed, Running connectivity test..."
+
 if ping -c 1 google.com &> /dev/null
 then
   echo "Conecction test Success"
